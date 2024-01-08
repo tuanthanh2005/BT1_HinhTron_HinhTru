9fe120fc4df4864a07e2231801a3fb28c1c64915
ADMIN <ADMIN@PC332>
2024-01-08 10:16:40 +0700

    Add project files.

---
+++ chuong1_KeThua/Hinh Tru.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chuong1_KeThua
+{
+    class Hinh_Tru : HinhTron
+    {
+        private double chieuCao;
+
+
+        public Hinh_Tru() : base()
+        {
+            chieuCao = 6;
+        }
+        public Hinh_Tru(double chieuCao,double banKinh): base(banKinh)
+        {
+            this.chieuCao = chieuCao;
+
+        }
+
+        public void setChieuCao(double chieuCao)
+        {
+            this.chieuCao = chieuCao;
+        }
+        public double getChieuCao()
+        {
+            return chieuCao;
+        }
+
+        public new double TinhDienTich()
+        {
+            return 2 * base.TinhDienTich() + TinhChuVi() * chieuCao;
+        }
+        public double TinhTheTich()
+        {
+            return base.TinhDienTich() * chieuCao;
+        }
+    }
+}
---
+++ chuong1_KeThua/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chuong1_KeThua
+{
+    class HinhTron
+    {
+        private double banKinh;
+
+
+
+        public HinhTron(): base()
+        {
+            banKinh = 5;
+        }
+        public HinhTron (double banKinh)
+        {
+            this.banKinh = Math.Abs(banKinh);
+        }
+        public void setBanKinh(double banKinh)
+        {
+            this.banKinh = banKinh;
+        }
+        public double getBanKinh()
+        {
+            return banKinh;
+        }
+        public double TinhDienTich()
+        {
+            return Math.PI * banKinh * banKinh;
+        }
+        public double TinhChuVi()
+        {
+            return Math.PI * 2 * banKinh;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HinhTron Htron = new HinhTron();
+            Console.WriteLine("Xuat dien Tich : {0}- Xuat Chu Vi : {1}",Htron.TinhDienTich(),Htron.TinhChuVi());
+            Hinh_Tru Htru = new Hinh_Tru();
+            Console.WriteLine("Xuat dien Tich : {0}- Xuat The Tich : {1}",Htru.TinhDienTich(),Htru.TinhTheTich());
+            Console.ReadKey();
+        }
+    }
+}
